bfd564c1657be032b8a5a3b68fdaf58f872d4676
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-06-30 22:40:54 +0700

    aa

--- FashionShop/src/main/java/com/java/fashionshop/component/OAuth2SuccessHandler.java
+++ FashionShop/src/main/java/com/java/fashionshop/component/OAuth2SuccessHandler.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import com.java.fashionshop.component.JwtUtil;
 import com.java.fashionshop.entity.UserEntity;
 import com.java.fashionshop.jpa.JpaUser;
 
