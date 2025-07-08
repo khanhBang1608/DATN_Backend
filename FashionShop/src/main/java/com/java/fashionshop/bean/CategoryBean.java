749d312ab5b0d6c2b08beebea234c9653b01b1bc
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-07-08 09:14:55 +0700

    Update CategoryBean.java

--- FashionShop/src/main/java/com/java/fashionshop/bean/CategoryBean.java
+++ FashionShop/src/main/java/com/java/fashionshop/bean/CategoryBean.java
@@ -17,7 +17,7 @@ public class CategoryBean {
     private String name;
 
     @NotNull(message = "Trạng thái không được để trống")
-    private Boolean status = true;
+    private Boolean status;
 
     private Integer parentId; // null nếu không có danh mục cha
 }
