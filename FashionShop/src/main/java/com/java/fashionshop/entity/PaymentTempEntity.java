53bd20ee584cb7f9467352303c9a501cb293e0c8
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-08-12 20:26:26 +0700

    aaa

--- FashionShop/src/main/java/com/java/fashionshop/entity/PaymentTempEntity.java
+++ FashionShop/src/main/java/com/java/fashionshop/entity/PaymentTempEntity.java
@@ -1,34 +1,34 @@
-package com.java.fashionshop.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "payment_temp")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class PaymentTempEntity {
-
-    @Id
-    @Column(name = "txn_ref", nullable = false, length = 50)
-    private String txnRef;
-
-    @Column(name = "order_id", nullable = false)
-    private Integer orderId;
-
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-
-}
-
+//package com.java.fashionshop.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "payment_temp")
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class PaymentTempEntity {
+//
+//    @Id
+//    @Column(name = "txn_ref", nullable = false, length = 50)
+//    private String txnRef;
+//
+//    @Column(name = "order_id", nullable = false)
+//    private Integer orderId;
+//
+//    @Column(name = "created_at", nullable = false)
+//    private LocalDateTime createdAt;
+//
+//
+//}
+//
--- FashionShop/src/main/java/com/java/fashionshop/jpa/JpaPaymentTemp.java
+++ FashionShop/src/main/java/com/java/fashionshop/jpa/JpaPaymentTemp.java
@@ -1,10 +1,10 @@
-package com.java.fashionshop.jpa;
-
-import com.java.fashionshop.entity.PaymentTempEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface JpaPaymentTemp extends JpaRepository<PaymentTempEntity, String> {
-    Optional<PaymentTempEntity> findByTxnRef(String txnRef);
-}
+//package com.java.fashionshop.jpa;
+//
+//import com.java.fashionshop.entity.PaymentTempEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface JpaPaymentTemp extends JpaRepository<PaymentTempEntity, String> {
+//    Optional<PaymentTempEntity> findByTxnRef(String txnRef);
+//}
